4e7749272d3139417dae573c1ddc46bbce2e3ee1
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-10-16 15:33:58 +0300

    s

---
+++ MemeMakerReal/MemeMakerReal/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MemeMakerReal
+//
+//  Created by Ziya Kok on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topsegmentedchoice: UISegmentedControl!
+    
+    @IBOutlet weak var bottomsegmentedchoice: UISegmentedControl!
+    
+
+    @IBOutlet weak var topsegmentedcontrol: UILabel!
+    
+    @IBOutlet weak var bottomsegment: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
