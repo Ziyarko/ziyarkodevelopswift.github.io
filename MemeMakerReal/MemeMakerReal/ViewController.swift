4b0968aa39758b45057975c0c4df8560652f3679
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-10-21 19:35:20 +0300

    MemeMaker

--- MemeMakerReal/MemeMakerReal/ViewController.swift
+++ MemeMakerReal/MemeMakerReal/ViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var topsegmentedchoice: UISegmentedControl!
-    
-    @IBOutlet weak var bottomsegmentedchoice: UISegmentedControl!
-    
-
-    @IBOutlet weak var topsegmentedcontrol: UILabel!
-    
-    @IBOutlet weak var bottomsegment: UILabel!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Topchange.removeAllSegments()
+        for choice in topchoices {
+            Topchange.insertSegment(withTitle: choice.emoji, at: topchoices.count, animated: false)
+        }
+        Topchange.selectedSegmentIndex = 0
+        
+        Bottomchange.removeAllSegments()
+        for choice in bottomchoices{
+            Bottomchange.insertSegment(withTitle: choice.emoji, at: topchoices.count, animated: false)
+            Bottomchange.selectedSegmentIndex = 0
+            updatecaptions()
+        }
     }
 
+    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var Topchange: UISegmentedControl!
+    
+    @IBAction func Topsegmentedcontrolchange(_ sender: Any) {
+        updatecaptions()
+    }
+    @IBAction func Bottomsegmentedcontrolchanged(_ sender: Any) {
+        updatecaptions()
+    }
+    @IBAction func dragtoptext(_ sender: UIPanGestureRecognizer) {
+    }
+    @IBAction func dragbottomtext(_ sender: UIPanGestureRecognizer) {
+    }
+    @IBOutlet weak var Bottomtext: UILabel!
+    @IBOutlet weak var Bottomchange: UISegmentedControl!
+    @IBOutlet weak var Toptext: UILabel!
+    let topchoices = [
+        CaptionOption(emoji: "😎" , caption: "You know what's cool?" ),
+        CaptionOption(emoji: "♥️", caption: "You know what i love?"),
+        CaptionOption(emoji: "💥", caption: "You know what make me mad?")
+    
+        
+    ]
+    let bottomchoices = [
+        CaptionOption(emoji: "😸" , caption: "Cats wearing hats" ),
+        CaptionOption(emoji: "🐶", caption: "Dogs carrying logs"),
+        CaptionOption(emoji: "🙈", caption: "Monkeys being funky?")
+    ]
+func updatecaptions() {
+    let topindex = Topchange.selectedSegmentIndex
+    Toptext.text = topchoices[topindex].caption
+    
+    let bottomindex = Bottomchange.selectedSegmentIndex
+    Bottomtext.text = bottomchoices[bottomindex].caption
+    
+}
 
 }
 
