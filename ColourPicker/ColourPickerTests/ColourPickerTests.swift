40b824740200d378dc91313cda28b88868851b0f
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-10-24 21:17:55 +0300

    ColourPicker

---
+++ ColourPicker/ColourPickerTests/ColourPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColourPickerTests.swift
+//  ColourPickerTests
+//
+//  Created by Ziya Kok on 24.10.2024.
+//
+
+import Testing
+@testable import ColourPicker
+
+struct ColourPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
